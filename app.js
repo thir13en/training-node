5a81b30a2617519663338acb0626edcec8ada269
thir13en <santilpzate@gmail.com>
2019-11-23 15:05:47 +0100

    my first node rest API

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// / -> Hi There
+app.get('/', (req, res) => {
+	res.send('Hi there!');
+});
+
+// /bye -> See ya
+app.get('/bye', (req, res) => res.send('see ya!'));
+
+// /dog -> MIEW
+app.get('/dog', (req, res) => res.send('MIEW!'));
+
+app.listen(3000, () => console.log('listening on port 3000'));
