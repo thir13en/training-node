13de18d8be4fb1fc6564a22d14e3fa6eb8bddc42
thir13en <santilpzate@gmail.com>
2019-11-24 21:25:46 +0100

    added wildcar route

--- app.js
+++ app.js
@@ -12,4 +12,6 @@ app.get('/bye', (req, res) => res.send('see ya!'));
 // /dog -> MIEW
 app.get('/dog', (req, res) => res.send('MIEW!'));
 
+app.get('*', (req, res) => res.send('YOU ARE THE BEST OUT THERE!'))
+
 app.listen(3000, () => console.log('listening on port 3000'));
