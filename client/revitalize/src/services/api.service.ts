099cbe696174e719a4f63034b8152bc34d41fb06
Santiago Lopez <santiago.lopez@i2cat.net>
2019-12-02 08:56:44 +0100

    achieved to add query params

--- client/revitalize/src/services/api.service.ts
+++ client/revitalize/src/services/api.service.ts
@@ -1,21 +1,34 @@
-import { Injectable } from "@angular/core";
-import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs';
 
-import { environment } from "../environments/environment";
-import { ENDPOINTS } from "../network";
+import { environment } from '../environments/environment';
+import { ENDPOINTS } from '../network';
 
- @Injectable()
+
+@Injectable()
 export class ApiService {
   constructor(
     private http: HttpClient,
   ) {}
 
   get(urlFragment: ENDPOINTS, payload: { param: string, value: string }[]): Observable<any> {
-    const params: HttpParams = new HttpParams();
-    params.set(payload[0].param, payload[0].value);
-    debugger;
+    const params = this.addQueryParams(payload);
+
     return this.http.get([environment.apiUrl, urlFragment].join('/'), { params });
   }
+
+  private addQueryParams(payload: { param: string, value: string }[]): HttpParams {
+    let params: HttpParams = new HttpParams();
+
+    payload.forEach((el: { param: string, value: string }) =>
+        params = params.keys().length ?
+            params.set(el.param, el.value) :
+            params.append(el.param, el.value)
+    );
+
+    return params;
+  }
+
 }
