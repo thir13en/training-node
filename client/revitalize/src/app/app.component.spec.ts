2a44afd35f8fb315186b02a76bd06e54669ed326
thir13en <santilpzate@gmail.com>
2019-12-14 12:33:11 +0100

    tests passing

--- client/revitalize/src/app/app.component.spec.ts
+++ client/revitalize/src/app/app.component.spec.ts
@@ -1,9 +1,12 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
 
 import { TestingModule } from './testing/testing.module';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [TestingModule],
@@ -11,22 +14,13 @@ describe('AppComponent', () => {
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.debugElement.componentInstance;
+  });
+
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'revitalize'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('revitalize');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('revitalize app is running!');
-  });
 });
--- client/revitalize/src/app/testing/testing.module.ts
+++ client/revitalize/src/app/testing/testing.module.ts
@@ -1,5 +1,20 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+
+import { API_SERVICE_MOCK_PROVIDER } from "./mocks/api.service.mock";
 
 
-@NgModule()
+@NgModule({
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientTestingModule,
+  ],
+  providers: [
+    API_SERVICE_MOCK_PROVIDER
+  ],
+})
 export class TestingModule {}
