be0aa16ab97ce069370ef26732442a8f42f27fd0
Santiago Lopez <santiago.lopez@i2cat.net>
2019-12-17 10:11:45 +0100

    small change in testing module

--- client/revitalize/src/app/features/massage/components/massages/massages.component.spec.ts
+++ client/revitalize/src/app/features/massage/components/massages/massages.component.spec.ts
@@ -1,17 +1,19 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { TestingModule } from '@app/testing/testing.module';
 import { MassagesComponent } from './massages.component';
 
 describe('MassagesComponent', () => {
   let component: MassagesComponent;
   let fixture: ComponentFixture<MassagesComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MassagesComponent ]
-    })
-    .compileComponents();
-  }));
+  beforeEach(async(
+    () => TestBed.configureTestingModule(
+      {
+        imports: [TestingModule],
+        declarations: [MassagesComponent],
+      }).compileComponents()
+  ));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(MassagesComponent);
@@ -19,7 +21,6 @@ describe('MassagesComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  it('should create', () => expect(component).toBeTruthy());
+
 });
