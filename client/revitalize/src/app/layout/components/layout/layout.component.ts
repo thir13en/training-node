0ff4145fba16633a7295aa9f89dd04b2f19b8a76
Santiago Lopez <santiago.lopez@i2cat.net>
2019-12-19 11:11:14 +0100

    received massages in massage component

--- client/revitalize/src/app/layout/components/layout/layout.component.ts
+++ client/revitalize/src/app/layout/components/layout/layout.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy, HostBinding } from '@angular/core';
 
 @Component({
-  selector: 'app-layout',
   templateUrl: './layout.component.html',
-  styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LayoutComponent implements OnInit {
