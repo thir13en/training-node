c7137f00b565c45afe3ab870d17a95a30e2084e8
Santiago Lopez <santiago.lopez@i2cat.net>
2019-12-23 11:15:20 +0100

    successfully routed massages create

--- client/revitalize/src/app/features/massages/components/massages-new/massages-new.component.ts
+++ client/revitalize/src/app/features/massages/components/massages-new/massages-new.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MassagesNewComponent implements OnInit {
-
+  // TODO: create mixin to dynamically add margins
   constructor() { }
 
   ngOnInit() {
