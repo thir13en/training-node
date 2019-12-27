b6af992cb620397038ec0954c2b6b15008796143
Santiago Lopez <santiago.lopez@i2cat.net>
2019-12-27 11:46:43 +0100

    finished massage create flow

--- client/revitalize/src/app/features/massages/components/massages-new/massages-new.component.ts
+++ client/revitalize/src/app/features/massages/components/massages-new/massages-new.component.ts
@@ -1,14 +1,15 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import copy from './massages-new.copy.json';
 
 import { ApiService } from '@services/api.service';
 import { ENDPOINTS } from '@app/network';
+import { routing } from '@app/routes';
 
 
 @Component({
   selector: 'app-massages-new',
   templateUrl: './massages-new.component.html',
-  styles: [],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MassagesNewComponent implements OnInit {
@@ -16,6 +17,7 @@ export class MassagesNewComponent implements OnInit {
 
   constructor(
     private apiService: ApiService,
+    private router: Router,
   ) {}
 
   ngOnInit() {
@@ -23,7 +25,10 @@ export class MassagesNewComponent implements OnInit {
   }
 
   addNewMassage(newMassage: { type: string, price: number, imgUrl: string }): void {
-    this.apiService.post(ENDPOINTS.MASSAGES, newMassage);
+    this.apiService.post(ENDPOINTS.MASSAGES, newMassage).subscribe(
+      (res: any) => this.router.navigateByUrl(routing.ROUTES.MASSAGES),
+      err => console.error(err),
+    );
   }
 
 }
---
+++ client/revitalize/src/app/routes/routes.ts
@@ -0,0 +1,11 @@
+export const ROUTE_FRAGMENTS: any = {
+  ROOT: '',
+  MASSAGES: 'massages',
+  NEW: 'new',
+};
+
+export const ROUTES: any = {
+  ROOT: '/',
+  MASSAGES: [ROUTE_FRAGMENTS.ROOT, ROUTE_FRAGMENTS.MASSAGES].join('/'),
+  MASSAGES_NEW: [ROUTE_FRAGMENTS.ROOT, ROUTE_FRAGMENTS.MASSAGES, ROUTE_FRAGMENTS.NEW].join('/')
+};
