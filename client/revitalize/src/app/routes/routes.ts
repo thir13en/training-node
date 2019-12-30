5408a485e2363d59de8c50827dc6f2cf88ecf036
Santiago Lopez <santiago.lopez@i2cat.net>
2019-12-30 11:44:53 +0100

    creating routes fragments inyectable

--- client/revitalize/src/app/routes/routes.ts
+++ client/revitalize/src/app/routes/routes.ts
@@ -1,3 +1,5 @@
+import { InjectionToken } from '@angular/core';
+
 export const ROUTE_FRAGMENTS: any = {
   ROOT: '',
   MASSAGES: 'massages',
@@ -11,3 +13,5 @@ export const ROUTES: any = {
   MASSAGES: [ROUTE_FRAGMENTS.ROOT, ROUTE_FRAGMENTS.MASSAGES].join('/'),
   MASSAGES_NEW: [ROUTE_FRAGMENTS.ROOT, ROUTE_FRAGMENTS.MASSAGES, ROUTE_FRAGMENTS.NEW].join('/')
 };
+
+// export const ROUTES_FRAGMENTS_INJECTABLE = new InjectionToken<any>('RoutesFragments', {factory: () => } )
