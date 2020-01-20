38af8939c9c4face6010e3b287f499c1dfcf86b3
Santiago Lopez <santiago.lopez@i2cat.net>
2020-01-20 12:45:42 +0100

    adding reactive form

--- client/revitalize/src/app/features/massages/components/massages-composer/massages-composer.component.ts
+++ client/revitalize/src/app/features/massages/components/massages-composer/massages-composer.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import copy from './massages-composer.copy.json';
 
 import { ApiService } from '@services/api.service';
 import { NetworkUtils } from '@app/network';
-import { routing } from '@app/routes';
+// import { routing } from '@app/routes';
 import { ROUTE_FRAGMENTS } from '@routes/routes';
 import { Observable } from 'rxjs';
 import { MassageModel } from '@core/models';
@@ -18,19 +19,28 @@ import { MassageModel } from '@core/models';
 export class MassagesComposerComponent implements OnInit {
   copy: any;
   // TODO: pass template driven form to reactive form
+  massageForm: FormGroup;
   // if we are in edit mode this will be true
   editMode: boolean;
   // the item we are editing in case we are in edit mode, otherwise undefined
   massage$: Observable<MassageModel>;
 
   constructor(
-    private router: Router,
+    // private router: Router,
     private route: ActivatedRoute,
+    private fb: FormBuilder,
     private apiService: ApiService,
   ) {}
 
   ngOnInit() {
     this.copy = copy;
+
+    this.massageForm = this.fb.group({
+      type: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      imageUrl: ['', [Validators.required]],
+      description: ['', [Validators.required]],
+    });
 
     const massageId: string = this.route.snapshot.params[ROUTE_FRAGMENTS.MASSAGE_IDENTIFIER.replace(':', '')];
 
@@ -40,11 +50,13 @@ export class MassagesComposerComponent implements OnInit {
     }
   }
 
-  addNewMassage(newMassage: { type: string, price: number, imgUrl: string }): void {
-    this.apiService.post({ path: NetworkUtils.ENDPOINTS.MASSAGES, payload: newMassage}).subscribe(
-      (res: any) => this.router.navigateByUrl(routing.ROUTES.MASSAGES),
-      err => console.error(err),
-    );
+  onSubmit(): void {
+    // TODO: add reactive form here
+    debugger;
+    // this.apiService.post({ path: NetworkUtils.ENDPOINTS.MASSAGES, payload: newMassage}).subscribe(
+    //   (res: any) => this.router.navigateByUrl(routing.ROUTES.MASSAGES),
+    //   err => console.error(err),
+    // );
   }
 
 }
