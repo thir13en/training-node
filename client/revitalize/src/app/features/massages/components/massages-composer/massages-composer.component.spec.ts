40ff6e7437d58f8a0a4fb066c1d60dec75562fe2
Santiago Lopez <santiago.lopez@i2cat.net>
2020-02-10 11:12:01 +0100

    added more test before taking task

--- client/revitalize/src/app/features/massages/components/massages-composer/massages-composer.component.spec.ts
+++ client/revitalize/src/app/features/massages/components/massages-composer/massages-composer.component.spec.ts
@@ -1,14 +1,17 @@
+import { ChangeDetectionStrategy } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+import { EMPTY } from 'rxjs';
 
 import { TestingModule } from '@app/testing/testing.module';
 import { MaterialFormControlsModule } from '@ui/angular-material/material-form-controls.module';
 import { MaterialLayoutModule } from '@ui/angular-material/material-layout.module';
+import { ApiService } from '@services/api.service';
 import { MassagesComposerComponent } from './massages-composer.component';
-import { By } from '@angular/platform-browser';
-import { ChangeDetectionStrategy } from '@angular/core';
 
 
-describe('MassagesComposerComponent', () => {
+fdescribe('MassagesComposerComponent', () => {
   let component: MassagesComposerComponent;
   let fixture: ComponentFixture<MassagesComposerComponent>;
 
@@ -47,6 +50,27 @@ describe('MassagesComposerComponent', () => {
     expect('Edit massage').toEqual(buttonText);
   });
 
-  // TODO: add testing to check form data is correctly gathered
+  it('should post on creation mode', () => {
+    const service = TestBed.inject(ApiService);
+    const spyPut = spyOn(service, 'put').and.returnValues(EMPTY);
+    const spyPost = spyOn(service, 'post').and.returnValues(EMPTY);
+
+    component.onSubmit();
+
+    expect(spyPut).not.toHaveBeenCalled();
+    expect(spyPost).toHaveBeenCalled();
+  });
+
+  it('should put on edit mode', () => {
+    const service = TestBed.inject(ApiService);
+    const spyPut = spyOn(service, 'put').and.returnValues(EMPTY);
+    const spyPost = spyOn(service, 'post').and.returnValues(EMPTY);
+
+    component.editMode = true;
+    component.onSubmit();
+
+    expect(spyPost).not.toHaveBeenCalled();
+    expect(spyPut).toHaveBeenCalled();
+  });
 
 });
